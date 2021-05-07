7d5cde56c40f2eab2b44f6018b3cc4b11f6eee44
Eric D. Rohler <erohler@progress.com>
2021-05-07 15:52:06 -0400

    add theming source

---
+++ theming-across-products/Post01/src/AngularApp/src/app/components/blank-1/blank-1.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blank-1.component',
+  templateUrl: './blank-1.component.html'
+})
+export class Blank1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
