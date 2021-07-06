75a080605d6337ae7b0bc71edd5c95a9f5440b87
Eric D. Rohler <development@ericrohler.com>
2021-07-06 15:16:39 -0400

    Add Web samples (#3)
    * init
    
    * add base theme assets
    
    * added wpf base theme hex codes
    
    * added wpf theme file
    
    * add angular sample project
    
    * add custom theme
    
    * update package.json & install& && git push origin --all
    
    * add blazor client
    
    * update blazor grid styles and sample

---
+++ theming-across-products/Post02/src/TBACS.BlazorGridCustomStyle/Shared/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBACS.BlazorGridCustomStyle.Shared
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int SupplierId { get; set; }
+        public int CategoryId { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
