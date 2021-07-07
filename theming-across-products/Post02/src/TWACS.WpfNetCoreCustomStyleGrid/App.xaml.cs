84a2dc2c7833dd1525723dae016b1a4363e28da2
Eric D. Rohler <development@ericrohler.com>
2021-07-07 11:31:41 -0400

    Finalized Theming Sample for Post 02 Publishing (#4)
    * add wpf sample
    
    * update wpf sample

---
+++ theming-across-products/Post02/src/TWACS.WpfNetCoreCustomStyleGrid/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.ColorEditor;
+
+namespace TWACS.WpfNetCoreCustomStyleGrid
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            MaterialPalette.Palette.AccentNormalColor = ColorConverter.ColorFromString("#FFFF9800");
+            MaterialPalette.Palette.AccentHoverColor = ColorConverter.ColorFromString("#FFFFA824");
+            MaterialPalette.Palette.AccentPressedColor = ColorConverter.ColorFromString("#FFFF4C00");
+            MaterialPalette.Palette.DividerColor = ColorConverter.ColorFromString("#1E000000");
+            MaterialPalette.Palette.IconColor = ColorConverter.ColorFromString("#FF000000");
+            MaterialPalette.Palette.MainColor = ColorConverter.ColorFromString("#FFFFFFFF");
+            MaterialPalette.Palette.MarkerColor = ColorConverter.ColorFromString("#FF000000");
+            MaterialPalette.Palette.ValidationColor = ColorConverter.ColorFromString("#FFD50000");
+            MaterialPalette.Palette.ComplementaryColor = ColorConverter.ColorFromString("#FFE0E0E0");
+            MaterialPalette.Palette.AlternativeColor = ColorConverter.ColorFromString("#FFF5F5F5");
+            MaterialPalette.Palette.MarkerInvertedColor = ColorConverter.ColorFromString("#FFFFFFFF");
+            MaterialPalette.Palette.PrimaryColor = ColorConverter.ColorFromString("#FFFAFAFA");
+            MaterialPalette.Palette.PrimaryNormalColor = ColorConverter.ColorFromString("#FF03A9F4");
+            MaterialPalette.Palette.PrimaryFocusColor = ColorConverter.ColorFromString("#FF1EB6FD");
+            MaterialPalette.Palette.PrimaryHoverColor = ColorConverter.ColorFromString("#FF1EB6FD");
+            MaterialPalette.Palette.PrimaryPressedColor = ColorConverter.ColorFromString("#FFFF4C00");
+            MaterialPalette.Palette.RippleColor = ColorConverter.ColorFromString("#FFFFFFFF");
+            MaterialPalette.Palette.ReadOnlyBackgroundColor = ColorConverter.ColorFromString("#00FFFFFF");
+            MaterialPalette.Palette.ReadOnlyBorderColor = ColorConverter.ColorFromString("#FFABABAB");
+            MaterialPalette.Palette.DialogBackgroundColor = ColorConverter.ColorFromString("#FFFFFFFF");
+            MaterialPalette.Palette.SelectedUnfocusedColor = ColorConverter.ColorFromString("#FFEEEEEE");
+            MaterialPalette.Palette.DividerSolidColor = ColorConverter.ColorFromString("#FFE1E1E1");
+            MaterialPalette.Palette.PrimaryOpacity = 0.87;
+            MaterialPalette.Palette.SecondaryOpacity = 0.54;
+            MaterialPalette.Palette.DisabledOpacity = 0.26;
+            MaterialPalette.Palette.DividerOpacity = 0.38;
+
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ theming-across-products/Post02/src/TWACS.WpfNetCoreCustomStyleGrid/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using TBACS.BlazorGridCustomStyle.Shared;
+
+namespace TWACS.WpfNetCoreCustomStyleGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static async Task<List<Product>> GetDataAsync()
+        {
+            string json = await new HttpClient().GetStringAsync("https://sampledataapi.azurewebsites.net/api/v1/Products");
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+
+        private async void RadGridView1_Loaded(object sender, RoutedEventArgs e)
+        {
+            RadGridView1.ItemsSource = await GetDataAsync();
+        }
+    }
+}
